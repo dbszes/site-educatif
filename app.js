3f71b61a9d28a8d53b5141fa0f49b682b80b3f49
dbszes <hamiedziyad@gmail.com>
2026-09-15 16:48:27 +0200

    Initialize Supabase client in app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+const SUPABASE_URL = "https://eumfudusjqcjbsukxcyv.supabase.co";
+const SUPABASE_KEY = "sb_publishable_e9J4xKfdIbIM2STAI4lReQ_Ks-d8owd";
 
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+console.log("Supabase connecté !");
